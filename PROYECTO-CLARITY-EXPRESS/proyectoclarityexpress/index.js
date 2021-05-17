31c56826bae6b99cbfb6c5f7976e926755c0c6d1
Manuel <manuel.lopez@usach.cl>
2021-05-17 11:48:27 -0400

    Dependencias instaladas:
    Mongoose: para realizar una conexion con MongoDB
    Nodemon: para actualizar el servidor de manera automatica tras un cambio en el código.
    Definición del puerto de la applicación.
    v2

--- PROYECTO-CLARITY-EXPRESS/proyectoclarityexpress/index.js
+++ PROYECTO-CLARITY-EXPRESS/proyectoclarityexpress/index.js
@@ -7,10 +7,13 @@ app.use(express.json());
 
 //Rutas
 app.get('/',(req,res)=>{
-    res.send("Welcome to Express");
+    res.json({
+        name: "Manuel"
+    })
 })
 
 // Servidor
-app.listen(3000,()=>{
+/*app.listen(3000,()=>{
     console.log("Server on port 3000");
-})
+})*/
+app.listen(3000,"192.168.0.4",()=>{console.log("Server on port 3000")})
